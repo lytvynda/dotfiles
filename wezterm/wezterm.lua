a97d8af12458222a170192c7614a3ddc634b9f2f
pumpkineater <43705653+lytvynda@users.noreply.github.com>
2024-08-06 10:45:49 +0300

    wezterm - maximize window right away

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,18 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
-
+local mux = wezterm.mux
 local config = {}
+
 -- Use config builder object if possible
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
+
+-- Maximize the window right away
+wezterm.on('gui-startup', function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
 
 -- Settings
 config.color_scheme = "Batman"
@@ -18,7 +25,7 @@ config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
---config.default_domain = "WSL:Ubuntu-24.04"
+-- config.default_domain = "WSL:Ubuntu-24.04"
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
